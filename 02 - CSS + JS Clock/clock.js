d5e3867041caf0ee1256c09ece8b7c010c8a5690
Toyin Dawudu <toyindawudu@gmail.com>
2020-09-06 12:26:00 +0100

    Completed the clock application

--- 02 - CSS + JS Clock/clock.js
+++ 02 - CSS + JS Clock/clock.js
@@ -1,11 +1,25 @@
 // DOM Manipulation 
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 // Functions
 
-// Set a date 
-
 function setDate(){
-  // console.log('Hi');
-}
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds/60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins/60) * 360) + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((mins/12)* 360) + 90;
+  hourHand.style.transform = `rotate)${hourDegrees}deg`;
+
+  console.log(minsDegrees);   
+  
+}
 setInterval(setDate, 1000);
