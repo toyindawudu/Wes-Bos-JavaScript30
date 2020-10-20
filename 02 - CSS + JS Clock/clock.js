90aafd478aceadb5e296087cbc425ab3277b58c4
Toyin Dawudu <toyindawudu@gmail.com>
2020-10-20 23:30:39 +0100

    Working through project 2

--- 02 - CSS + JS Clock/clock.js
+++ 02 - CSS + JS Clock/clock.js
@@ -1,25 +1,28 @@
 // DOM Manipulation 
 const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
+const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 // Functions
 
 function setDate(){
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondDegrees = ((seconds/60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+  
   const mins = now.getMinutes();
   const minsDegrees = ((mins/60) * 360) + 90;
-  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  minsHand.style.transform = `rotate(${minsDegrees})deg`;
 
   const hour = now.getHours();
-  const hourDegrees = ((mins/12)* 360) + 90;
-  hourHand.style.transform = `rotate)${hourDegrees}deg`;
+  const hourDegrees = ((hour/12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}) deg`;
 
-  console.log(minsDegrees);   
+  // console.log(minsDegrees);   
   
 }
-setInterval(setDate, 1000);
+
+setInterval(setDate, 1000);  
